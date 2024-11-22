d39dd9a4e7df33cdd062c31f8fbd087e3d77c977
Karina <krin_ka@mail.ru>
2024-11-22 11:14:25 +0300

    Application commit

--- app/src/main/java/com/karinaksenovskaya/myapplication/WeatherApp.kt
+++ app/src/main/java/com/karinaksenovskaya/myapplication/WeatherApp.kt
@@ -4,14 +4,12 @@ import android.app.Application
 import com.karinaksenovskaya.myapplication.di.ApplicationComponent
 import com.karinaksenovskaya.myapplication.di.DaggerApplicationComponent
 
-class
-WeatherApp : Application() {
+class WeatherApp : Application() {
 
     lateinit var applicationComponent: ApplicationComponent
 
     override fun onCreate() {
         super.onCreate()
-
         applicationComponent = DaggerApplicationComponent.factory().create(this)
     }
 }
